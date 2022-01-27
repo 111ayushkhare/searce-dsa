aadf14637c575d68052c4f94d498f1bf7e373654
Ayush Khare <ayushkhare.dev23@gmail.com>
2022-01-27 14:14:32 +0530

    array implementaion of queue and its operations done

---
+++ src/week3day3/practice/queue/QueueArrayImplementation.java
@@ -0,0 +1,55 @@
+package week3day3.practice.queue;
+
+class QueueArrayImplementation {
+    private final int CAPACITY;
+    private int front, rear;
+    private final int[] qArr;
+
+    QueueArrayImplementation(int maxElements) {
+        front = -1;
+        rear = -1;
+        CAPACITY = maxElements;
+        qArr = new int[CAPACITY];
+    }
+
+    // Method to add an element in the queue
+    boolean enqueue(int val) {
+        // Condition to check if queue is full
+        // then element cannot be added, return false
+        if (isQFull()) { return false; }
+
+        // Condition if element is added to empty queue
+        if (isQEmpty()) {
+            qArr[++front] = val;
+            rear++;
+            return true;
+        }
+
+        // When queue is neither empty nor full, simply add the element
+        qArr[++rear] = val;
+        return true;
+    }
+
+    // Method to remove an element from the queue
+    int dequeue() {
+        if(isQEmpty()) { return Integer.MIN_VALUE; }
+        return qArr[front++];
+    }
+
+    // Method to retrieve the element at front end
+    // If queue is empty, Integer.MIN_VALUE is returned
+    int frontElement() { return isQEmpty() ? Integer.MIN_VALUE : qArr[front]; }
+
+    // Method to retrieve the element at rear end
+    // If queue is empty, Integer.MIN_VALUE is returned
+    int rearElement() { return isQEmpty() ? Integer.MIN_VALUE : qArr[rear]; }
+
+    // Method to find number of elements in the queue
+    int qSize() { return isQEmpty() ? 0 : rear - front + 1; }
+
+    // Method to check if the queue is full
+    boolean isQFull() { return rear == CAPACITY - 1; }
+
+    // Method to check if the queue is empty
+    boolean isQEmpty() { return front == -1 && rear == -1; }
+}
---
+++ src/week3day3/practice/queue/TestQueue.java
@@ -0,0 +1,53 @@
+package week3day3.practice.queue;
+
+import inputreader.InputReader;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class TestQueue {
+    public static void main(String[] args) throws IOException {
+        // Reading input from a text file
+        File input = new File("src/week3day3/practice/queue/input.txt");
+        FileWriter output = new FileWriter("src/week3day3/practice/queue/output.txt");
+        InputReader ir = new InputReader(new BufferedReader(new FileReader(input)));
+
+        int T = ir.nextInt();
+        for (int t = 0; t < T; t++) {
+            QueueArrayImplementation queue = new QueueArrayImplementation(ir.nextInt());
+
+            output.write("Testcase " + (t + 1) + ":- \n");
+            output.write("---------------------------\n");
+
+            int numOfItems = ir.nextInt();
+            for (int i = 0; i < numOfItems; i++) {
+                if (queue.enqueue(ir.nextInt())) {
+                    output.write("Item enqueued: " + queue.rearElement() + "\n");
+                }
+            }
+
+            output.write("Queue size: " + queue.qSize() + " \n");
+            output.write("Front element: " + queue.frontElement() + " \n");
+            output.write("Rear element: " + queue.rearElement() + " \n");
+
+            output.write("\nItem Dequeued: " + queue.dequeue() + " \n");
+            output.write("Queue size: " + queue.qSize() + " \n");
+            output.write("Front element: " + queue.frontElement() + " \n");
+            output.write("Rear element: " + queue.rearElement() + " \n");
+
+            if (queue.enqueue(ir.nextInt())) {
+                output.write("\nItem enqueued: " + queue.rearElement() + " \n");
+                output.write("Queue size: " + queue.qSize() + " \n");
+                output.write("Front element: " + queue.frontElement() + " \n");
+                output.write("Rear element: " + queue.rearElement() + " \n");
+            }
+
+            output.write("\n");
+        }
+
+        output.close();
+    }
+}
